e67b5d045e3617a95f8e07cf6cd7c0b13e8883f9
Kate <solntseva_kate@mail.ru>
2024-11-09 20:06:32 +0700

    main activity logic finished

--- app/src/main/java/com/example/lab11/MainActivity.kt
+++ app/src/main/java/com/example/lab11/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.lab11
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
 
@@ -16,7 +17,23 @@ class MainActivity : AppCompatActivity() {
         val checkButton = findViewById<Button>(R.id.button_check)
         val resultText = findViewById<TextView>(R.id.result_text)
 
+        checkButton.setOnClickListener {
+            val input = input_text.text.toString().trim()
 
+            if (input.length != 1) {
+                Toast.makeText(this, "Введите 1 символ", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (input[0].isUpperCase()) {
+                when (input[0]) {
+                    'L', 'M', 'K', 'D' -> resultText.text = "Это согласная буква"
+                    else -> resultText.text = "Гласная буква"
+                }
+            } else {
+                Toast.makeText(this, "Введите латинскую заглавную букву", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     }
 }
